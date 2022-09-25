3b45612257488c01f6da7612674021db5d3a072e
SalsabillaDwiOktalia <dwioktalia86@gmail.com>
2022-09-25 23:48:21 +0700

    tugas7_js

---
+++ js/hewan.js
@@ -0,0 +1,134 @@
+$("document").ready(function () {
+    //hewan-hewan unggas
+    $(".ayam").click(function () {
+        $(this).css({
+            background: "url('img/ayam.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/ayam.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".burungPuyuh").click(function () {
+        $(this).css({
+            background: "url('img/burungpuyuh.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/burungpuyuh.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".bebek").click(function () {
+        $(this).css({
+            background: "url('img/bebek.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/bebek.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".burungPipit").click(function () {
+        $(this).css({
+            background: "url('img/burungpipit.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/burung.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //hewan-hewan ternak
+    $(".sapi").click(function () {
+        $(this).css({
+            background: "url('img/sapi.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/sapi.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".kambing").click(function () {
+        $(this).css({
+            background: "url('img/kambing.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/kambing.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".babi").click(function () {
+        $(this).css({
+            background: "url(https://i.pinimg.com/564x/cf/e9/0c/cfe90c86cfcf6cb6301ec6fe355b2bb6.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/babi.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".kuda").click(function () {
+        $(this).css({
+            background: "url('img/kuda.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/kuda.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //Hewan-Hewan Buas
+    $(".singa").click(function () {
+        $(this).css({
+            background: "url('img/singa.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/singa.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".harimau").click(function () {
+        $(this).css({
+            background: "url('img/harimau.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/harimau.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".gajah").click(function () {
+        $(this).css({
+            background: "url('img/gajah.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/ayam.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+    //
+    $(".anjing").click(function () {
+        $(this).css({
+            background: "url('img/anjing.jpg')"
+        });
+        $("<audio></audio>").attr({
+            'src': 'audio/ayam.mp3',
+            'volume': 0.4,
+            'autoplay': 'autoplay'
+        }).appendTo("body");
+    });
+        });
